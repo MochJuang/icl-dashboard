2f394be5b9fe95703b804ccb4c0ea00302d2affb
MochJuang <mochjuangpp@gmail.com>
2026-01-19 23:47:51 +0700

    change api

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1';
+export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://0.tcp.ap.ngrok.io:12409/api/v1';
 
 export const WALLET_TYPE_LABELS: Record<string, string> = {
     REGULAR: 'Regular',
